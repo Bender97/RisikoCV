78317497d90d44a262f15bf0645d381d5b8a6756
Bender97 <bopallino@gmail.com>
2021-12-14 23:38:23 +0100

    HSV thresholding integrated. Now working on ratio between the size of the map and the army size

--- src/main.cpp
+++ src/main.cpp
@@ -12,40 +12,60 @@ using namespace cv;
 
 using namespace cv::xfeatures2d;
 
-std::string rect_map = "/home/fusy/Documents/risiko/imgs/rect_map.jpg";
-std::string test_map = "/home/fusy/Documents/risiko/imgs/IMG_20211214_110513.jpg";
-//std::string test_map = "/home/fusy/Documents/risiko/imgs/IMG_20211214_110540.jpg"; // difficult: map is not well localized
-std::string india_map = "/home/fusy/Documents/risiko/imgs/rect_map_india.jpg";
-
 class Match {
 
-public:
-    Match() {
 
-        Mat img_object = imread( rect_map, IMREAD_GRAYSCALE );
-        Mat img_scene = imread( test_map, IMREAD_GRAYSCALE );
-        Mat india = imread( india_map, IMREAD_GRAYSCALE );
+    std::string rect_map = "/home/fusy/Documents/risiko/imgs/rect_map.jpg";
+    std::string test_map = "/home/fusy/Documents/risiko/imgs/IMG_20211214_110943.jpg";        // india has 2
+//    std::string test_map = "/home/fusy/Documents/risiko/imgs/IMG_20211214_110513.jpg";        // india has 0
+//    std::string test_map = "/home/fusy/Documents/risiko/imgs/IMG_20211214_110627.jpg";          // india has 1
+//    std::string test_map = "/home/fusy/Documents/risiko/imgs/IMG_20211214_110540.jpg"; // difficult: map is not well localized
+    std::string india_map = "/home/fusy/Documents/risiko/imgs/rect_map_india.jpg";
+    Mat img_object;
+    Mat img_scene;
+    Mat img_scene_color;
+    Mat india;
+
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+    Mat descriptors_object, descriptors_scene;
+
+    std::vector<DMatch> good_matches;
+    Mat img_matches;
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+
+    // Homography
+    Mat H;
+
+    std::vector<Point2f> obj_corners;
+    std::vector<Point2f> scene_corners;
+
+    void loadImages() {
+        img_object = imread( rect_map, IMREAD_GRAYSCALE );
+        img_scene = imread( test_map, IMREAD_GRAYSCALE );
+        img_scene_color = imread( test_map, IMREAD_COLOR );
+        india = imread( india_map, IMREAD_GRAYSCALE );
 
         if ( img_object.empty() || img_scene.empty() )
         {
             cout << "Could not open or find the image!\n" << endl;
             return ;
         }
-        //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+    }
+
+    void loadKeypointsAndDescriptors() {
         int minHessian = 400;
         Ptr<SURF> detector = SURF::create( minHessian );
-        std::vector<KeyPoint> keypoints_object, keypoints_scene;
-        Mat descriptors_object, descriptors_scene;
         detector->detectAndCompute( img_object, noArray(), keypoints_object, descriptors_object );
         detector->detectAndCompute( img_scene, noArray(), keypoints_scene, descriptors_scene );
-        //-- Step 2: Matching descriptor vectors with a FLANN based matcher
-        // Since SURF is a floating-point descriptor NORM_L2 is used
+    }
+
+    void findMatches() {
         Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
         std::vector< std::vector<DMatch> > knn_matches;
         matcher->knnMatch( descriptors_object, descriptors_scene, knn_matches, 2 );
         //-- Filter matches using the Lowe's ratio test
         const float ratio_thresh = 0.68f;
-        std::vector<DMatch> good_matches;
         for (size_t i = 0; i < knn_matches.size(); i++)
         {
             if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
@@ -53,34 +73,23 @@ public:
                 good_matches.push_back(knn_matches[i][0]);
             }
         }
-        //-- Draw matches
-        Mat img_matches;
-        drawMatches( img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
-                     Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-        //-- Localize the object
-        std::vector<Point2f> obj;
-        std::vector<Point2f> scene;
         for( size_t i = 0; i < good_matches.size(); i++ )
         {
             //-- Get the keypoints from the good matches
             obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
             scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
         }
-        Mat H = findHomography( obj, scene, RANSAC );
-        //-- Get the corners from the image_1 ( the object to be "detected" )
-        std::vector<Point2f> obj_corners(4);
+    }
+
+    void computeObjectCorners() {
+        obj_corners.resize(4);
         obj_corners[0] = Point2f(0, 0);
         obj_corners[1] = Point2f( (float)img_object.cols, 0 );
         obj_corners[2] = Point2f( (float)img_object.cols, (float)img_object.rows );
         obj_corners[3] = Point2f( 0, (float)img_object.rows );
-        std::vector<Point2f> scene_corners(4);
-        perspectiveTransform( obj_corners, scene_corners, H);
+    }
 
-//        Mat map = img_scene.clone();
-//        Mat mask = Mat::zeros( img_scene.rows, img_scene.cols, img_scene.type() );
-//        Mat warp_mat = getAffineTransform( obj_corners.data(), scene_corners.data() );
-//        warpAffine( india, mask, warp_mat, india.size() );
-
+    void drawLines() {
         //-- Draw lines between the corners (the mapped object in the scene - image_2 )
         line( img_matches, scene_corners[0] + Point2f((float)img_object.cols, 0),
               scene_corners[1] + Point2f((float)img_object.cols, 0), Scalar(0, 255, 0), 4 );
@@ -90,13 +99,56 @@ public:
               scene_corners[3] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
         line( img_matches, scene_corners[3] + Point2f((float)img_object.cols, 0),
               scene_corners[0] + Point2f((float)img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-        //-- Show detected matches
+    }
 
+public:
+    Match() {
+
+        loadImages();
+
+        //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+        loadKeypointsAndDescriptors();
+
+        //-- Step 2: Matching descriptor vectors with a FLANN based matcher
+        // Since SURF is a floating-point descriptor NORM_L2 is used
+        findMatches();
+
+        //-- Draw matches
+        drawMatches( img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1),
+                     Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+        //-- Localize the object
+        H = findHomography( obj, scene, RANSAC );
+
+        //-- Get the corners from the image_1 ( the object to be "detected" )
+        computeObjectCorners();
+
+        // compute corners in scene image frame
+        scene_corners.resize(4);
+        perspectiveTransform( obj_corners, scene_corners, H);
+
+        drawLines();
+
+        //-- Show detected matches
         cv::resize(img_matches, img_matches, cv::Size(img_matches.cols/2, img_matches.rows/2));
         imshow("Good Matches & Object detection", img_matches );
         waitKey();
 
+
+
+
+        Mat th;
+        Mat img_scene_hsv;
+        cvtColor(img_scene_color, img_scene_hsv, COLOR_BGR2HSV);
+        inRange(img_scene_hsv, Scalar(110, 142, 68), Scalar(141, 255, 255), th);
+        cv::resize(th, th, cv::Size(th.cols/2, th.rows/2));
+        imshow("thresholded", th );
+        waitKey();
+
+
+
         cv::Mat projected = img_scene.clone();
+
+        size_t pix_cont = 0;
 
         for (int r = 0; r<india.rows; r++) {
             for (int c=0; c<india.cols; c++) {
@@ -107,18 +159,19 @@ public:
                     std::vector<Point2f> world_corners;
                     perspectiveTransform(camera_corners, world_corners, H);
                     projected.at<uchar>((int) world_corners[0].y, (int) world_corners[0].x) = 255;
+                    if (th.at<uchar>((int) world_corners[0].y, (int) world_corners[0].x)==255) {
+                        pix_cont++;
+                    }
                 }
 
             }
         }
 
+        cout << "PIX_CONT: " << pix_cont << endl;
+
         cv::resize(projected, projected, cv::Size(projected.cols/2, projected.rows/2));
         imshow("second", projected );
         waitKey();
-    //        cv::resize(mask, mask, cv::Size(mask.cols/2, mask.rows/2));
-    //        imshow("second", mask );
-    //        waitKey();
-        return;
     }
 
 };
